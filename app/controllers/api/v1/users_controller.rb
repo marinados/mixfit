feaf45850b69112387f5d8ed4d8386b363695647
marina starkova <marina.starkova@gmail.com>
2019-08-16 15:25:50 +0200

    user endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,18 @@
 class UsersController < ApiBaseController 
 	
+	def update
+		current_user.attributes = user_params
+		if current_user.save
+			render(json: current_user.to_json)
+		else
+			render(json: current_user.errors.to_json, status: :bad_request)
+		end
+	end
+
+	private
+
+	def user_params
+		params.permit(:height, :weight)
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,23 @@
 class User < ApplicationRecord
 	has_many :daily_activities
 	has_many :personal_recipes
+	has_many :food_intakes
 
 	validates :name, 
 					presence: true, 
 					uniqueness: true, 
 					length: { minimum: 5 }
+
+=begin
+Since we don't have the info on the precision of
+the height mesurement we need, let's assume that we 
+have full centimeters as value. Same goes for the weight.
+Should we ever support higher precision and/or other measure
+units, this would need to be reviewed (and I would make a builder
+to support multiple formats).
+Just for precaution limiting the height to 250 cm. Didn't come up
+with any weight restrictions.
+=end
+	validates :height, numericality: { less_than_or_equal_to: 250 }, allow_nil: true
+	validates :weight, numericality: true, allow_nil: true
 end
