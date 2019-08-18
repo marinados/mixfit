d9480f06409f33dcb1b81136c9681e2d2a4684da
marina starkova <marina.starkova@gmail.com>
2019-08-18 21:25:51 +0200

    Add a JSON helper to parse json response

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,13 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 
-Dir['support/*.rb'].each {|file| require file }
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |file| require file }
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -61,4 +62,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include JSONHelper, type: :request
 end
